8def1592008e7c89115d141e0743ade73630fd84
nueapop <nueapop.mo@gmail.com>
2022-07-30 15:04:48 +0700

    update: main_amg8833.py

--- main_amg8833.py
+++ main_amg8833.py
@@ -3,7 +3,7 @@ import machine
 import adafruit_amg8833
 
 machine.freq(240000000)
-i2c = machine.I2C(scl=machine.Pin(15), sda=machine.Pin(4), freq=400000)
+i2c = machine.I2C(0, scl=machine.Pin(19), sda=machine.Pin(21), freq=400000)
 amg = adafruit_amg8833.AMG8833(i2c, addr=0x69)
 time.sleep(1)
 
