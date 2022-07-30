74a8a690e147d0248f7b6c0ed4036a33044e96a2
nueapop <nueapop.mo@gmail.com>
2022-07-30 15:10:59 +0700

    update: main_mlx90640.py

--- main_mlx90640.py
+++ main_mlx90640.py
@@ -3,7 +3,7 @@ import machine
 import adafruit_mlx90640
 
 machine.freq(240000000)
-i2c = machine.I2C(0, scl=machine.Pin(22, machine.Pin.OUT), sda=machine.Pin(21, machine.Pin.OUT), freq=400000)
+i2c = machine.I2C(0, scl=machine.Pin(23, machine.Pin.OUT), sda=machine.Pin(22, machine.Pin.OUT), freq=400000)
 mlx = adafruit_mlx90640.MLX90640(i2c)
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 frame = [0] * 768
