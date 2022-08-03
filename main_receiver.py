604acda789b033d8f7bd8d6645dbe2fca7ace6d7
nueapop <nueapop.mo@gmail.com>
2022-08-04 00:09:07 +0700

    update: main_receiver.py

--- main_receiver.py
+++ main_receiver.py
@@ -1,9 +1,8 @@
-import time
-from machine import Pin
-from ir_receiver import NEC_16
+import machine
+import ir_receiver
 
 def callback(data, addr, ctrl):
-    if data > 0:  # NEC protocol sends repeat codes.
+    if data > 0:
         print('Data 0x{:02x} Addr 0x{:04x}'.format(data, addr))
 
-ir = NEC_16(Pin(15, Pin.IN), callback)
+ir = ir_receiver.NEC_16(machine.Pin(15, machine.Pin.IN), callback)
