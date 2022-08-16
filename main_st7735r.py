e90f2fe0b91c9fc058a7d4c18d21e8abd1328724
nueapop <nueapop.mo@gmail.com>
2022-08-16 18:32:19 +0700

    update: Alls File

--- main_st7735r.py
+++ main_st7735r.py
@@ -2,6 +2,5 @@ import machine
 import adafruit_st7735r
 
 tft = adafruit_st7735r.TFT(machine.SPI(2, baudrate=20000000, polarity=0, phase=0, sck=machine.Pin(14), mosi=machine.Pin(13)), 16, 17, 18)
-tft.initr()
 tft.rgb(True)
 tft.fill(adafruit_st7735r.TFT.RED)
