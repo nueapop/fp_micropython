317631f6f91394480a706e0e4a0489bd210e212f
nueapop <nueapop.mo@gmail.com>
2022-08-16 09:57:53 +0700

    update: Alls File

--- main_receiver.py
+++
@@ -1,8 +0,0 @@
-import machine
-import ir_receiver
-
-def callback(data, addr, ctrl):
-    if data > 0:
-        print('Data 0x{:02x} Addr 0x{:04x}'.format(data, addr))
-
-ir = ir_receiver.NEC_16(machine.Pin(15, machine.Pin.IN), callback)
